2d26d662e54e4e6439e32fb23d8fed71e7e8783e
Sala-Profesores <Sala-Profesores@PC-SALAPF2.iberotec.edu.pe>
2015-06-06 08:25:54 -0500

    Tercer Commit

--- src/proyectogrupal/ProyectoGrupal.java
+++ src/proyectogrupal/ProyectoGrupal.java
@@ -12,6 +12,7 @@ public class ProyectoGrupal {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo!!!");
+        System.out.println("....");
     }
     
 }
