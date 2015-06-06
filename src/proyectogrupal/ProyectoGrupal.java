953dc7d87068432db45b6659e2e5ae7842a29a5d
JCAMAYO <>
2015-06-06 08:18:05 -0500

    Primer commit JC

---
+++ src/proyectogrupal/ProyectoGrupal.java
@@ -0,0 +1,16 @@
+package proyectogrupal;
+
+/**
+ *
+ * @author Sala-Profesores
+ */
+public class ProyectoGrupal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
