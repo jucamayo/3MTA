c45918577b0aa2f9a48f5611251e256f50bf5942
QT <>
2015-06-08 10:14:02 -0500

    Quinto Commit

--- src/proyectogrupal/ProyectoGrupal.java
+++ src/proyectogrupal/ProyectoGrupal.java
@@ -10,9 +10,9 @@ public class ProyectoGrupal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola Mundo!!!");
         System.out.println("....");
+        System.out.println("Desde QT - TR");
     }
     
 }
